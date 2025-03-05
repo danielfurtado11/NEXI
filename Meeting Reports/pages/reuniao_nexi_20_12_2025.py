cb7c5b371fc8e1957604c475540b2b09a49024b7
Daniel Furtado <danielfrutado@gmail.com>
2025-03-05 02:04:09 +0000

    oioi

--- Meeting Reports/pages/reuniao_nexi_20_12_2025.py
+++
@@ -1,7 +0,0 @@
-import streamlit as st
-
-st.title("Relatório: Reunião NEXI (20-12-2024)")
-st.write("Aqui está o conteúdo detalhado do relatório...")
-
-if st.button("Voltar"):
-    st.switch_page("main.py")
